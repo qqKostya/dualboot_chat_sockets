78ec55e4ce1b8843c062b83e3f97945013b95fee
Kazakov Kostya <leap74rus@gmail.com>
2023-04-22 08:21:44 +0300

    add: new fitch

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from threading import Thread
+import threading
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -7,17 +7,21 @@ PORT = 12345
 clients = set()
 
 
-def new_client(connection: socket.socket) -> None:
+def new_client(connection: socket.socket, address: tuple) -> None:
     with connection:
         while True:
             data = connection.recv(1024)
             if not data:
                 break
-            print(f"Received {data}")
+
+            with open("logs.txt", "a") as f:
+                f.write(
+                    f"Received message '{data.decode()}' from {address[0]}:{address[1]}\n"
+                )
 
             for client in clients:
                 if client is not connection:
-                    connection.sendall(b"Hello client")
+                    client.sendall(data)
 
     clients.remove(connection)
 
@@ -25,8 +29,13 @@ def new_client(connection: socket.socket) -> None:
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
+
     while True:
-        conn, _ = s.accept()
+        conn, addr = s.accept()
+        print(f"New client connected: {addr}")
+
         clients.add(conn)
-        client_thread = Thread(target=new_client, args=(conn,))
+
+        client_thread = threading.Thread(target=new_client, args=(conn, addr))
         client_thread.start()
